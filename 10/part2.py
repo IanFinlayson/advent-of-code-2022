53d93dc94b5bed00b348ea5bd246a68df948013a
Ian Finlayson <ifinlay@umw.edu>
2022-12-11 01:07:25 +0000

    Day 10 done

---
+++ 10/part2.py
@@ -0,0 +1,51 @@
+import sys
+
+class Machine:
+    def __init__(self, prog):
+        self.prog = prog
+        self.pc = 0
+        self.cycle = 1
+        self.reg = 1
+        self.latch = None
+
+    def tick(self):
+        # print the CRT screen
+        xpos = (self.cycle - 1) % 40
+
+        if (self.reg >= xpos - 1) and (self.reg <= xpos + 1):
+            print("#", end="")
+        else:
+            print(".", end="")
+        if (self.cycle + 0) % 40 == 0:
+            print()
+        
+        # latch the result from last time
+        if self.latch != None:
+            self.reg += self.latch
+            self.latch = None
+        else:
+            # fetch
+            instruction = self.prog[self.pc]
+            self.pc += 1
+            
+            # execute
+            if instruction[0:4] == "noop":
+                pass
+            else:
+                amt = int(instruction[5:])
+                self.latch = amt
+        
+        self.cycle += 1
+
+# load the program
+prog = []
+for line in sys.stdin:
+    prog.append(line[:-1])
+m = Machine(prog)
+
+# step through the cycles we care about
+while m.cycle <= 240:
+    m.tick()
+
+print()
+
